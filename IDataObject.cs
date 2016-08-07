1fbc0b9509d3297a723ff9b15fbf3b293636a287
Glen Husman <wappokickingdeer@gmail.com>
2016-08-07 12:40:29 -0700

    Added UNTESTED JSON backend implementation

--- IDataObject.cs
+++ IDataObject.cs
@@ -1,9 +1,11 @@
 ﻿namespace DataConnector
 {
-    public interface IDataObject
+	public interface IDataObject
     {
         /// <summary>
         /// Gets the unique identifier of this object in the data backend.
+		/// 
+		/// The setting capabilities for this property may be more stringent than specified here. Such requirements are backend-specific.
         /// </summary>
         int ID { get; }
 
@@ -11,8 +13,10 @@
         /// Gets a value indicating whether this value has been committed to the backend, or in other words,
         /// returns true if and only if an identifier has been assigned to this object from the backend.
         /// Note that this may return true if unsaved changes have been made, but will not return true if the ID is not assigned.
+		/// 
+		/// The setting capabilities for this property may be more stringent than specified here. Such requirements are backend-specific.
         /// </summary>
-        bool IsStoredData { get; }
+		bool IsStoredData { get; }
         
         // IDataBackend<IDataObject> DataBackend { get; }
     }
--- JSON/JsonDataBackend.cs
+++ JSON/JsonDataBackend.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace DataConnector.JSON
 {
@@ -17,6 +23,8 @@ namespace DataConnector.JSON
 			private set;
 		}
 
+		protected readonly JsonSerializer Serializer;
+
 		public JsonDataBackend (string filePath)
 		{
 			if (filePath == null) {
@@ -24,6 +32,186 @@ namespace DataConnector.JSON
 			}
 
 			FilePath = filePath;
+
+			Serializer = new JsonSerializer ();
+			Serializer.ContractResolver = new JsonDataObjectContractResolver (this);
+			Serializer.Converters.Add (new DataObjectJsonConverter (this));
+		}
+
+		/// <summary>
+		/// A contract resolver that selects fields in an opt-in mode.
+		/// </summary>
+		class JsonDataObjectContractResolver : DefaultContractResolver
+		{
+			public JsonDataObjectContractResolver (IDataBackend<IDataObject> backend)
+			{
+				Backend = backend;
+			}
+
+			IDataBackend<IDataObject> Backend;
+
+			protected override IList<JsonProperty> CreateProperties (Type type, MemberSerialization memberSerialization)
+			{
+				// TODO this is a bit hacky
+
+				var props = base.CreateProperties (type, MemberSerialization.Fields);
+
+				foreach (var p in props) {
+					p.Writable = true;
+					p.Readable = true;
+
+					if (typeof(IDataObject).IsAssignableFrom (p.PropertyType)) {
+						// TODO ensure this doesn't call excessively recursively
+						p.Converter = new DataObjectJsonConverter (Backend);
+					}
+				}
+
+				return props;
+			}
+		}
+
+		class DataObjectJsonConverter : JsonConverter
+		{
+
+			protected IDataBackend<IDataObject> Backend;
+
+			public DataObjectJsonConverter (IDataBackend<IDataObject> backend)
+			{
+				Backend = backend;
+			}
+
+			public override bool CanConvert (Type objectType)
+			{
+				return typeof(IDataObject).IsAssignableFrom (objectType);
+			}
+
+			public override object ReadJson (JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+			{
+				if (!typeof(IDataObject).IsAssignableFrom (objectType)) {
+					throw new ArgumentException ("The given object is not an IDataObject");
+				}
+
+				// We wrote an ID, allow an exception if something's not right
+				int id = reader.ReadAsInt32 ().Value;
+
+				// TODO strongly type the name
+				var method = Backend.GetType ().GetMethod ("GetObjectByID").MakeGenericMethod (objectType);
+				return method.Invoke (Backend, new Object[]{ id });
+			}
+
+			public override void WriteJson (JsonWriter writer, object value, JsonSerializer serializer)
+			{
+				if (value is IDataObject) {
+					// Just write an ID
+					writer.WriteValue (((IDataObject)value).ID);
+				} else {
+					throw new ArgumentException ("The given object is not an IDataObject");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The dictionary containing type-separated dictionaries of IDs to objects.
+		/// This collection contains all objects.
+		/// The string key is the type full name.
+		/// </summary>
+		protected IDictionary<string, IDictionary<int, IDataObject>> AllObjects = new Dictionary<string, IDictionary<int, IDataObject>> ();
+
+		public void SaveObject (IDataObject target)
+		{
+			IDictionary<int, IDataObject> objectTypeCollection;
+			if (!AllObjects.TryGetValue (target.GetType ().FullName, out objectTypeCollection)) {
+				objectTypeCollection = new Dictionary<int, IDataObject> ();
+				AllObjects [target.GetType ().FullName] = objectTypeCollection;
+			}
+
+			if (!target.IsStoredData) {
+				// TODO document these requirements
+				int newId = 1;
+				// TODO a bit of a hack
+				while (objectTypeCollection.ContainsKey (newId)) {
+					newId++;
+				}
+
+				// TODO these requirements should be documented
+				target.GetType ().GetProperty ("ID", BindingFlags.Public | BindingFlags.SetProperty | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy).SetValue (target, newId);
+				target.GetType ().GetProperty ("IsStoredData", BindingFlags.Public | BindingFlags.SetProperty | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy).SetValue (target, true);
+			}
+
+			objectTypeCollection [target.ID] = target;
+
+			SaveToBackend ();
+		}
+
+		protected void SaveToBackend ()
+		{
+			using (FileStream stream = File.Open (FilePath, FileMode.Create)) {
+				using (StreamWriter writer = new StreamWriter (stream)) {
+					Serializer.Serialize (writer, AllObjects);
+				}
+			}
+		}
+
+		protected void LoadBackend ()
+		{
+			using (FileStream stream = File.Open (FilePath, FileMode.Open)) {
+				using (StreamReader srdr = new StreamReader (stream)) {
+					using (JsonReader reader = new JsonTextReader (srdr)) {
+						AllObjects = Serializer.Deserialize<IDictionary<string, IDictionary<int, IDataObject>>> (reader);
+					}
+				}
+			}
+		}
+
+		public TObject GetObjectByID<TObject> (int id) where TObject : IDataObject
+		{
+			IDataObject result = null;
+			try {
+				result = AllObjects [typeof(TObject).FullName] [id];
+			} catch {
+				// TODO fancy foreign keys (those that have a composite type as opposed to an ID) may cause a StackOverflowException here
+				LoadBackend ();
+
+				result = AllObjects [typeof(TObject).FullName] [id];
+			}
+
+			return (TObject)result;
+		}
+
+		public IEnumerable<TObject> GetAllObjectsOfType<TObject> () where TObject : IDataObject
+		{
+			return AllObjects [typeof(TObject).FullName].Values.Cast<TObject> ();
+		}
+
+		public IEnumerable<TChildObject> GetChildrenOf<TParentObject, TChildObject> (TParentObject parent)
+			where TParentObject : IDataObject
+			where TChildObject : IDataObject
+		{
+			// TODO this is intended to be able to use more efficient implementations
+			// This is just a hack
+			return GetAllObjectsOfType<TChildObject> ().Where (c => {
+				foreach (var mem in c.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)) {
+					// TODO move this to a different namespace
+					DataConnector.SQL.ForeignKeyAttribute fkey;
+					if ((fkey = ((DataConnector.SQL.ForeignKeyAttribute)Attribute.GetCustomAttribute (mem, typeof(DataConnector.SQL.ForeignKeyAttribute)))) != null) {
+						if (fkey.TargetTable == typeof(TParentObject)) {
+							var fkeyValue = mem.GetValue (c);
+							// Match foreign key relations
+							return parent.Equals (fkeyValue) || parent.ID.Equals (fkeyValue);
+						}
+					}
+				}
+
+				// No foreign key relation found
+				// Do not include in return
+				return false;
+			});
+		}
+
+		public void Dispose ()
+		{
+			// TODO fix
+			// this is a no-op
 		}
 	}
 }
