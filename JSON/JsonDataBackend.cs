4e2431e00b02b08b99d5aaf22a1d1de5c2394f31
Glen Husman <wappokickingdeer@gmail.com>
2016-08-06 18:15:44 -0700

    Add framework for JSON backend

---
+++ JSON/JsonDataBackend.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DataConnector.JSON
+{
+	/// <summary>
+	/// Uses Json.NET as a data storage backend. Target objects are expected to have the appropriate attributes.
+	/// </summary>
+	public class JsonDataBackend : IDataBackend<IDataObject>
+	{
+		/// <summary>
+		/// Gets the path of the backend file.
+		/// </summary>
+		/// <value>The backend file path.</value>
+		public string FilePath {
+			get;
+			private set;
+		}
+
+		public JsonDataBackend (string filePath)
+		{
+			if (filePath == null) {
+				throw new ArgumentNullException ("filePath");
+			}
+
+			FilePath = filePath;
+		}
+	}
+}
+
