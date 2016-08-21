9628a8a853b9bfafd8442df33bb4ad6497058ad7
Glen Husman <wappokickingdeer@gmail.com>
2016-08-21 16:40:09 -0700

    Add StoredDataAttribute

--- SQL/SqlFieldAttribute.cs
+++ SQL/SqlFieldAttribute.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataConnector.SQL
 {
+    [Obsolete("Will be replaced with a backend-agnostic storage attribute identifier")]
 	[AttributeUsage (AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
 	public sealed class SqlFieldAttribute : Attribute
 	{
---
+++ StoredDataAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConnector
+{
+    /// <summary>
+    /// Represents that a property or field is stored data.
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public sealed class StoredDataAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the name of the field or column to use in the backend.
+        /// If this is unset or null, the literal name of the member will be used.
+        /// </summary>
+        public string StoredName { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the data type to use to store the value in the backend.
+        /// If this is unset or null, defaults will be used.
+        /// </summary>
+        public System.Data.DbType? DataType { get; set; }
+
+        public StoredDataAttribute()
+        {
+        }
+    }
+}
