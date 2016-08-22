1ae6f294a2240d5f959f9fb78cc8fbd0c4bbf9c2
Glen Husman <wappokickingdeer@gmail.com>
2016-08-21 17:42:01 -0700

    Update IDataObject docs to reflect setter requirements

--- IDataObject.cs
+++ IDataObject.cs
@@ -4,8 +4,7 @@
     {
         /// <summary>
         /// Gets the unique identifier of this object in the data backend.
-		/// 
-		/// The setting capabilities for this property may be more stringent than specified here. Such requirements are backend-specific.
+		/// This property must have a setter, however it does not have to be publicly visible.
         /// </summary>
         int ID { get; }
 
@@ -13,8 +12,7 @@
         /// Gets a value indicating whether this value has been committed to the backend, or in other words,
         /// returns true if and only if an identifier has been assigned to this object from the backend.
         /// Note that this may return true if unsaved changes have been made, but will not return true if the ID is not assigned.
-		/// 
-		/// The setting capabilities for this property may be more stringent than specified here. Such requirements are backend-specific.
+		/// This property must have a setter, however it does not have to be publicly visible.
         /// </summary>
 		bool IsStoredData { get; }
         
